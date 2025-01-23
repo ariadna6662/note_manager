fd2e56809ae659bb509d0cd3d829508b9bc5ac1d
ariadna6662 <mayorovaee@mail.ru>
2025-01-23 15:53:36 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,22 @@
+# Grade 1. Этап 1: Задание 1 #
+
+username = 'Ivan'
+print("Имя пользователя:", username)
+
+title = 'Домашнее задание'
+print("Заголовок заметки:", title)
+
+content = 'Выполнить задание по математике'
+print("Описание заметки:", content)
+
+status = 'В процессе'
+print("Статус заметки:", status)
+
+created_date = '10-01-2025'
+print("Дата создания заметки:", created_date)
+
+issue_date = '15-01-2025'
+print("Дата истечения заметки (дедлайн):", issue_date)
+
+
+
