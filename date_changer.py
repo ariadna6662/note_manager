286595eb399eb6818210232cf6c1f86aeb95dd76
ariadna6662 <mayorovaee@mail.ru>
2025-01-25 17:56:12 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,9 @@
+created_date = '12-03-2024'
+created_date2 = '12-03-2024'
+print('Оригинальная дата: ', created_date)
+print('Преобразованная дата: ', created_date2[0:5])
+
+issue_date = '12 марта 2024'
+issue_date2 = '12 марта 2024'
+print('Оригинальная дата: ', issue_date)
+print('Преобразованная дата: ', issue_date2[0:8])
